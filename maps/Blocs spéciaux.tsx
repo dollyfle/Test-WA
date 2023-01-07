4141e8e4761f03e108658d21e3a0129b1c210cd9
DollyRamella <108594195+DollyRamella@users.noreply.github.com>
2023-01-07 17:31:26 +0100

    Création de la carte Carte 1

---
+++ maps/Blocs spéciaux.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Blocs spéciaux" tilewidth="32" tileheight="32" tilecount="80" columns="8">
+ <image source="../tilesets/roads.png" width="256" height="320"/>
+</tileset>
---
+++ tilesets/Fond.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Fond" tilewidth="32" tileheight="32" tilecount="1444" columns="38">
+ <image source="exterior.png" width="1240" height="1240"/>
+</tileset>
